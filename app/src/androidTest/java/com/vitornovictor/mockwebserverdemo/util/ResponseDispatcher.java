1e119c8f95100c37e5706d96be6d2491aae19d0f
Vitor Nunes <vitornovictor@gmail.com>
2017-01-23 20:02:39 +0000

    Use descriptive names

--- app/src/androidTest/java/com/vitornovictor/mockwebserverdemo/util/ResponseDispatcher.java
+++ app/src/androidTest/java/com/vitornovictor/mockwebserverdemo/util/ResponseDispatcher.java
@@ -13,41 +13,41 @@ public class ResponseDispatcher extends Dispatcher {
     String username = parts[parts.length - 1];
 
     if (username.equalsIgnoreCase(Parameters.VALID_USER_1)) {
-      return buildForBody(ServerResponse.buildFor(Parameters.VALID_USER_1,
-                                                  Parameters.VALID_USER_1_ID));
+      return buildResponseForBody(ServerResponse.buildFor(Parameters.VALID_USER_1,
+                                                          Parameters.VALID_USER_1_ID));
     }
 
     if (username.equalsIgnoreCase(Parameters.NONEXISTENT_USER)) {
-      return buildForResponseCode(Parameters.RESPONSE_CODE_USER_NOT_FOUND);
+      return buildResponseForCode(Parameters.RESPONSE_CODE_USER_NOT_FOUND);
     }
 
     if (username.equalsIgnoreCase(Parameters.TIMEOUT_USER)) {
       String body = ServerResponse.buildFor(Parameters.TIMEOUT_USER, Parameters.TIMEOUT_USER_ID);
 
-      return buildForBodyWithDelay(body,
-                                   Parameters.BYTES_PER_PERIOD,
-                                   Parameters.DELAY_PERIOD,
-                                   TimeUnit.SECONDS);
+      return buildResponseForBodyWithDelay(body,
+                                           Parameters.BYTES_PER_PERIOD,
+                                           Parameters.DELAY_PERIOD,
+                                           TimeUnit.SECONDS);
     }
 
     if (username.equalsIgnoreCase(Parameters.MALFORMED_USER)) {
-      return buildForBody(ServerResponse.getMalformedResponse());
+      return buildResponseForBody(ServerResponse.getMalformedResponse());
     }
 
     return null;
   }
 
-  private MockResponse buildForBody(String body) {
+  private MockResponse buildResponseForBody(String body) {
     return new MockResponse().setBody(body);
   }
 
-  private MockResponse buildForBodyWithDelay(
+  private MockResponse buildResponseForBodyWithDelay(
       String body, long bytesPerPeriod, long period, TimeUnit unit) {
 
     return new MockResponse().setBody(body).throttleBody(bytesPerPeriod, period, unit);
   }
 
-  private MockResponse buildForResponseCode(int responseCode) {
+  private MockResponse buildResponseForCode(int responseCode) {
     return new MockResponse().setResponseCode(responseCode);
   }
 }
